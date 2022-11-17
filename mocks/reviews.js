45663ea89f92ccea214759cf22fac11a5e4c882c
fess986u <fess986@mail.ru>
2022-11-17 13:56:52 +0300

    Add static mocks for films #19
    - add files for ts types in mock
    - add films mock for use it in project

---
+++ mocks/reviews.js
@@ -0,0 +1,24 @@
+const reviews = [
+  {
+    id: 1,
+    user: {
+      id: 3,
+      name: 'Kate Muir',
+    },
+    rating: 8.9,
+    comment: 'Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director`s funniest and most exquisitely designed movies in years.',
+    date: '2019-05-08T14:13:56.569Z',
+  },
+  {
+    id: 2,
+    user: {
+      id: 2,
+      name: 'Kit Harington',
+    },
+    rating: 8.9,
+    comment: 'Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director`s funniest and most exquisitely designed movies in years.',
+    date: '2019-05-08T14:13:52.569Z',
+  },
+];
+
+export default reviews;
---
+++ project/src/types/mocks-types.ts
@@ -0,0 +1,19 @@
+export type Film = {
+  id: number,
+  name: string;
+  posterImage: string;
+  previewImage: string;
+  backgroundImage: string;
+  backgroundColor: string;
+  videoLink: string;
+  previewVideoLink: string;
+  description: string;
+  rating: number;
+  scoresCount: number;
+  director: string;
+  starring: string[];
+  runTime:number;
+  genre: string;
+  released: number;
+  isFavorite: boolean;
+}
