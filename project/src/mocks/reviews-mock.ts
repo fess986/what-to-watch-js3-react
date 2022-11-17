a258d18bfed8de6307dcb70de50f2d059b64c3d6
fess986u <fess986@mail.ru>
2022-11-17 14:30:42 +0300

    Add static mocks for reviews #19
    - add type for review
    - add mock for reviews

---
+++ project/src/mocks/reviews-mock.ts
@@ -0,0 +1,25 @@
+import { Review } from '../types/mocks-types';
+
+export const reviews: Review[] = [
+  {
+    id: 1,
+    user: {
+      id: 4,
+      name: 'my name',
+    },
+    rating: 9.0,
+    comment: 'very good film i think!',
+    date: '2019-05-08T14:13:56.569Z',
+  },
+
+  {
+    id: 2,
+    user: {
+      id: 4,
+      name: 'Piter Parker',
+    },
+    rating: 1.0,
+    comment: 'it was the worst film i had ever seen!!',
+    date: '2019-05-08T14:13:52.569Z',
+  },
+];
--- project/src/types/mocks-types.ts
+++ project/src/types/mocks-types.ts
@@ -17,3 +17,14 @@ export type Film = {
   released: number;
   isFavorite: boolean;
 }
+
+export type Review = {
+    id: number,
+    user: {
+      id: number,
+      name: string,
+    },
+    rating: number,
+    comment: string,
+    date: string,
+}
