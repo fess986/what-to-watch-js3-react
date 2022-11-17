87bc74acde0ee484a8cea3a018a4f88475d9bac2
fess986u <fess986@mail.ru>
2022-11-17 18:13:41 +0300

    Add new component "films list" #26
    - add component "films list"
    - include component "film card" in it
    - render cards from array of films from props

---
+++ project/src/components/film-list/film-list.tsx
@@ -0,0 +1,28 @@
+// компонент списка карточек фильмов
+import React from 'react';
+import FilmCard from '../film-card/film-card';
+import { Film } from '../../types/mocks-types';
+
+type FilmListProps = {
+  films: Film[];
+}
+
+// компонент получает массив фильмов и рендерит их
+function FilmList(props: FilmListProps): JSX.Element {
+
+  const {films} = props;
+
+  return (
+    <div className="catalog__films-list">
+
+      {
+        films.map((film : Film) : JSX.Element => (
+          <FilmCard films = {film} key = {film.id}/>
+        ))
+      }
+
+    </div>
+  );
+}
+
+export default FilmList;
