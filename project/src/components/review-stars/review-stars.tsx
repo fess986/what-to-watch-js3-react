9cadeab57bc0f07a6e203f4e05d50bcc74113a61
fess986u <fess986@mail.ru>
2022-12-02 10:13:12 +0300

    Add review page basic logic #62
    - add component for stars in project
    - add logic for stars count
    - save stars count in component add-review

---
+++ project/src/components/review-stars/review-stars.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import {ChangeEvent} from 'react';
+
+type ReviewStarsProps = {
+  starCount : number;
+  onStarClick : (count: number) => void;
+}
+
+const ReviewStars = function(props : ReviewStarsProps) {
+
+  const {starCount, onStarClick} = props;
+
+  const starClickHandler = (event : ChangeEvent<HTMLInputElement>) => {
+    onStarClick(Number(event.target.value));
+  };
+
+  const starsContent = [];
+
+  for (let i = 10; i > 0; i--) {
+
+    starsContent.push(<input onChange={starClickHandler} checked={starCount === i} key={Math.random() + i} className="rating__input" id={`star-${i}`} type="radio" name="rating" value={i} />);
+
+    starsContent.push(<label key={Math.random() + i} className="rating__label" htmlFor={`star-${i}`}>Rating {i}</label>);
+  }
+
+  return (
+    <div className="rating">
+      <div className="rating__stars">
+
+        {starsContent}
+
+      </div>
+    </div>
+  );
+};
+
+export default ReviewStars;
