90c66c0fb8b7dc3dacb5dfc5251f437accfe47a8
fess986u <fess986@mail.ru>
2022-12-15 16:33:24 +0300

    Update Logo component #74
    - now it can work not only in main, but also in footer

--- project/src/components/logo/Logo.tsx
+++ project/src/components/logo/Logo.tsx
@@ -1,12 +1,22 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import {AppRoute} from '../../const/const';
-import React from 'react';
 
-function Logo(): JSX.Element {
+type LogoProps = {
+  lightness? : string;
+}
+
+function Logo(props? : LogoProps | undefined): JSX.Element {
+  let logoStyles = 'logo__link';
+
+  if (props && props.lightness === 'light') {
+    logoStyles = 'logo__link logo__link--light';
+  }
+
   return (
 
     <div className="logo">
-      <Link to={AppRoute.Main} className="logo__link">
+      <Link to={AppRoute.Main} className={logoStyles}>
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>
         <span className="logo__letter logo__letter--3">W</span>
