4dc3cd295ad4fc656324424b02be322f34bd12b3
fess986u <fess986@mail.ru>
2023-02-13 18:14:34 +0300

    Use History object  to use it in actions #141
    - create History object from 'history'
    - create wrapper for Router from react-router-dom to
    configurate it with history object

---
+++ project/src/browser-history.ts
@@ -0,0 +1,6 @@
+import { createBrowserHistory } from 'history';
+
+const browserHistory = createBrowserHistory();
+console.log(browserHistory);
+
+export default browserHistory;
---
+++ project/src/components/history-route/history-route.tsx
@@ -0,0 +1,38 @@
+// тут мы создаем обертку над Router, где опишем необходимые параметры для него через объект истории, созданный из библиотеки 'history'.
+
+import React, {useState, useLayoutEffect} from 'react';
+import { Router } from 'react-router-dom';
+import type {BrowserHistory} from 'history';
+
+export interface HistoryRouterProps {
+  history: BrowserHistory // объект истории из пакета 'history'
+  basename?: string
+  children?: React.ReactNode
+}
+
+function HistoryRouter({
+  basename,
+  children,
+  history,
+}: HistoryRouterProps) : JSX.Element {
+  const [state, setState] = useState({ // параметры для обновления истории
+    action: history.action,
+    location: history.location,
+  });
+
+  // работает как и обычный useEffect, но асинхронно, после того как страница уже отрисуется. Редко применяется, когда это оправданно - тут не понятно, оправдано ли это
+  useLayoutEffect(() => history.listen(setState), [history]); // при смене history, меняем параметры истории
+
+  return (
+    <Router
+      basename={basename}
+      location={state.location}
+      navigationType={state.action}
+      navigator={history}
+    >
+      {children}
+    </Router>
+  );
+}
+
+export default HistoryRouter;
