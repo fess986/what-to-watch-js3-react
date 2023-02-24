5a843f591b52542bdfdbe17963f229250d9a04a4
fess986u <fess986@mail.ru>
2023-02-24 17:44:50 +0300

    Add tests in Git Actions #182

--- project/src/utils/utils.ts
+++ project/src/utils/utils.ts
@@ -7,8 +7,6 @@ export const getDuration = (runTime: number) : string => {
   return `${Math.floor(runTime / 60)}h ${Math.floor(runTime % 60)}m`;
 };
 
-console.log(getDuration(75.5));
-
 export const parseCommentDate = (date: string): string => {
   const parsedDate = moment(date).format('MMMM DD[,] YYYY');
   return parsedDate;
