c8b9f3597607e1e6163e9e94b70d68a7fc0679c4
fess986u <fess986@mail.ru>
2023-02-25 12:10:10 +0300

    utils.ts tests #180
    - parseCommentDate testing

---
+++ project/src/utils/mocks.ts
@@ -0,0 +1,12 @@
+import {system, internet} from 'faker';
+import faker from 'faker';
+
+export const fakerObject = {
+  system: system.filePath(),
+  number: faker.datatype.number(),
+  name: faker.name.title(),
+  internet: internet.avatar(),
+  date: faker.datatype.datetime(),
+};
+
+export {};
--- project/src/utils/utils.test.ts
+++ project/src/utils/utils.test.ts
@@ -1,34 +1,67 @@
-import { getDuration } from './utils';
-//export const getDuration = (runTime: number) : string => `${Math.floor(runTime / 60)}h ${runTime % 60}m`;
+import { getDuration, parseCommentDate } from './utils';
+import {fakerObject} from './mocks';
 
-describe('Function: getDuration', () => {
-  it('should return string "1h 1m"', () => {
+let globalTestValue : number;
+
+describe('Utils functiions tests', () => {
+
+  describe('Function: getDuration', () => {
+
+    beforeEach(() => { // инициализируется перед каждым запуском
+      globalTestValue = 30; // объявляем без CONST
+    });
+
+    afterAll(() => { // после каждого
+      globalTestValue = 31; // в принципе это бесмысленно, так как перед каждым случаем переопределяем
+    });
+
+    it('should return string "1h 1m"', () => { // тут описываем конкретные тесты
     // Тестовый случай
-    const testValue = 61;
-    expect(getDuration(testValue)).toBe('1h 1m');
-    expect(getDuration(testValue)).toEqual('1h 1m');
+      const testValue = 61;
+      expect(getDuration(testValue)).toBe('1h 1m'); // ожидаем чтобы выражение в expect было равно '1h 1m'
+      expect(getDuration(testValue)).not.toBe(null); // обратное утверждение
+      expect(getDuration(testValue)).toEqual('1h 1m'); // схожа по работе с toBe , но при этом не так строго сравнивает, например 2 одинаковых объекта можно сравнить, и чисто поля будут сравниваться
+    });
+
+    it('should return string "0h 30m"', () => {
+    // Тестовый случай
+      expect(getDuration(globalTestValue)).toBe('0h 30m');
+    });
+
+    test('should return something', ()=> {
+      expect(getDuration(fakerObject.number)).toBeDefined();
+    });
+
+    test('should return string "0h 0m"', () => { // тут тоже описываем конкретные тесты
+    // Тестовый случай
+      const testValue = -10;
+      globalTestValue = 0;
+
+      expect(getDuration(testValue)).toBe('0h 0m');
+      expect(getDuration(globalTestValue)).toBe('0h 0m');
+    });
+
+    test('should return string "1h 15m"', () => {
+    // Тестовый случай
+      const testValue = 75.5;
+      expect(getDuration(testValue)).toBe('1h 15m');
+    });
   });
 
-  it('should return string "0h 30m"', () => {
-    // Тестовый случай
-    const testValue = 30;
-    expect(getDuration(testValue)).toBe('0h 30m');
+  describe('Function: parseCommentDate', () => {
+
+    test('should return something', ()=> {
+      expect(parseCommentDate('2019-05-08T14:13:56.569Z')).toBeDefined();
+    });
+
+    test('should return May 08, 2019', ()=> {
+      expect(parseCommentDate('2019-05-08T14:13:56.569Z')).toBe('May 08, 2019');
+    });
+
+    test('should return not Array Prototype', ()=> {
+      expect(parseCommentDate('2019-05-08T14:13:56.569Z')).not.toBeInstanceOf(Array);
+    });
+
   });
 
-  test('should return string "0h 0m"', () => {
-    // Тестовый случай
-    const testValue = -10;
-    const testValue2 = 0;
-
-    expect(getDuration(testValue)).toBe('0h 0m');
-    expect(getDuration(testValue2)).toBe('0h 0m');
-  });
-
-  test('should return string "1h 15m"', () => {
-    // Тестовый случай
-    const testValue = 75.5;
-    expect(getDuration(testValue)).toBe('1h 15m');
-  });
 });
-
-export {};
