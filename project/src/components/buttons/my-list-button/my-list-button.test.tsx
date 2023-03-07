602f7b0bc0b81e323cbc475b39b7a3ffa086101d
fess986u <fess986@mail.ru>
2023-03-07 18:55:39 +0300

    Component MyListButton tests  #216

---
+++ project/src/components/buttons/my-list-button/my-list-button.test.tsx
@@ -0,0 +1,52 @@
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import {Routes, Route} from 'react-router-dom';
+
+import MyListButton from './my-list-button';
+import HistoryRouter from '../../../components/history-route/history-route';
+import { AppRoute } from '../../../const/const';
+
+const history = createMemoryHistory();
+
+describe('MyListButton Component tests', () => {
+
+  it(('should render correctly'), () => {
+
+    render(
+      <HistoryRouter history={history}>
+        <MyListButton />
+      </HistoryRouter>
+    );
+
+    expect(screen.getByRole('button')).toHaveTextContent('My list');
+
+  });
+
+  it('should redirect correctly', async () => {
+    history.push('/this');
+
+    render(
+      <HistoryRouter history={history}>
+        <Routes>
+          <Route
+            path='/this'
+            element= {<MyListButton />}
+          />
+          <Route
+            path={AppRoute.MyList}
+            element= {<h1>another page</h1>}
+          />
+        </Routes>
+      </HistoryRouter>
+    );
+
+    expect(screen.getByRole('button')).toHaveTextContent('My list');
+
+    await userEvent.click(screen.getByRole('button'));
+
+    expect(screen.getByText('another page')).toBeInTheDocument();
+
+  });
+});
+
